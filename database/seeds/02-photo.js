02f4d1a4af514a44a61004981d80b547a4ec450d
Olga Cortez <olgalataviacortez@gmail.com>
2019-11-20 15:45:18 -0600

    Added more seeded data to db for FE

--- database/seeds/02-photo.js
+++ database/seeds/02-photo.js
@@ -12,6 +12,10 @@ exports.seed = function(knex) {
         {id: 2, twitter_user_id: 2, pic: "https://random.dog/8f969962-5ca9-418c-95e0-7b37817294b1.jpg"},
         {id: 3, twitter_user_id: 3, pic: "https://random.dog/554ebddb-ca7b-4e45-954f-0b7e1f509538.jpg"},
         {id: 4, twitter_user_id: 4, pic: "https://random.dog/9606265b-59eb-422f-b840-5b23ad8272bc.jpg"},
+        {id: 5, twitter_user_id: 5, pic: "https://random.dog/2ee86ac3-9bc3-49bb-998d-1bb7a1a913e9.jpg"},
+        {id: 6, twitter_user_id: 6, pic: "https://random.dog/f5e0bd76-1103-4c71-bc86-7cd576587042.jpg"},
+        {id: 7, twitter_user_id: 7, pic: "https://random.dog/29152151-f563-45c6-984a-e0fdba531729.jpg"},
+        {id: 8, twitter_user_id: 8, pic: "https://random.dog/f886b15c-69d6-4840-bfe9-fd8442dcb516.jpg"},
       ]);
     });
 }
--- twitter/twitter-router.js
+++ twitter/twitter-router.js
@@ -4,11 +4,14 @@ const twitterRouter = require('express').Router();
 const restricted = require('../auth/restricted-middleware');
 
 const photoArray = [
-  "https://random.dog/ef14cef7-5568-4e10-b477-1e16a73f8692.mp4",
   "https://random.dog/8f969962-5ca9-418c-95e0-7b37817294b1.jpg",
   "https://random.dog/b85abb5b-5b9e-47d2-9938-71129cdfdb50.jpg",
   "https://random.dog/554ebddb-ca7b-4e45-954f-0b7e1f509538.jpg",
-  "https://random.dog/9606265b-59eb-422f-b840-5b23ad8272bc.jpg"
+  "https://random.dog/9606265b-59eb-422f-b840-5b23ad8272bc.jpg",
+  "https://random.dog/2ee86ac3-9bc3-49bb-998d-1bb7a1a913e9.jpg",
+  "https://random.dog/f5e0bd76-1103-4c71-bc86-7cd576587042.jpg",
+  "https://random.dog/29152151-f563-45c6-984a-e0fdba531729.jpg",
+  "https://random.dog/f886b15c-69d6-4840-bfe9-fd8442dcb516.jpg"
 ];
 
 
